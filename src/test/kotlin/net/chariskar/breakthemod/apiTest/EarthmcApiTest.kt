49d78a7849c127a34a04bc02c9c0f1e97c3ea390
Charilaos Karametos <charis.karametos@gmail.com>
2026-02-07 18:31:04 +0200

    add emc api tests

--- src/test/kotlin/net/chariskar/breakthemod/apiTest/EarthmcApiTest.kt
+++ src/test/kotlin/net/chariskar/breakthemod/apiTest/EarthmcApiTest.kt
@@ -2,14 +2,12 @@ package net.chariskar.breakthemod.apiTest;
 
 import kotlinx.coroutines.runBlocking
 import net.chariskar.breakthemod.Main
-import net.chariskar.breakthemod.TestScope
 import net.chariskar.breakthemod.client.api.Fetch
+import net.chariskar.breakthemod.client.objects.Nation
+import net.chariskar.breakthemod.client.objects.Resident
 import net.chariskar.breakthemod.client.objects.StaffList
-import net.chariskar.breakthemod.client.utils.Config
-import org.junit.jupiter.api.Assertions
+import net.chariskar.breakthemod.client.objects.Town
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertIs
 
@@ -19,11 +17,38 @@ import kotlin.test.assertIs
 class EarthmcApiTest {
 
     @Test
-    fun `test Get request`() {
+    fun `test GET request`() {
         runBlocking {
             val result = Fetch.getRequest<StaffList>(Main.config.staffRepoUrl)
             assertNotNull(result)
             assertIs<StaffList>(result)
+        }
+    }
+
+    @Test
+    fun `test towns`() {
+        runBlocking {
+            val result = Fetch.getTown("paris")
+            assertNotNull(result)
+            assertIs<Town>(result)
+        }
+    }
+
+    @Test
+    fun `test nations`() {
+        runBlocking {
+            val result = Fetch.getNation("france")
+            assertNotNull(result)
+            assertIs<Nation>(result)
+        }
+    }
+
+    @Test
+    fun `test players`() {
+        runBlocking {
+            val result = Fetch.getResident("charis_k")
+            assertNotNull(result)
+            assertIs<Resident>(result)
         }
     }
 
